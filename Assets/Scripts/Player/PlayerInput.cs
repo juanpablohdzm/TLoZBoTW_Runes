78f9665f33173f3757d9c98f811bcd828122f976
JuanPabloH <juan@3diq.io>
2020-07-02 15:51:15 -0500

    Create player input, player controller and start mocking the main scene.

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerInput : MonoBehaviour, IPlayerInput
+{
+    public Vector2 LeftThumbStick => OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+    public Vector2 RightThumbStick => OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+}
