f238c352d4f6f2d686018214855723d942b5160d
JuanPabloH <juan@3diq.io>
2020-07-02 15:53:35 -0500

    Make the player input a singleton.

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -6,4 +6,21 @@ public class PlayerInput : MonoBehaviour, IPlayerInput
 {
     public Vector2 LeftThumbStick => OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
     public Vector2 RightThumbStick => OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+    
+    public PlayerInput Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            if(Instance != this)
+                Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
 }
