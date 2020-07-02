1201391139560bfba75ecedf2c01825ad6fbca90
JuanPabloH <juan@3diq.io>
2020-07-02 15:05:23 -0500

    Add art, helpers, create first scene.

---
+++ Assets/Scripts/Helpers/CoroutineUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Utilities {
+    public static class CoroutineUtils { 
+        public static IEnumerator UntilEither(IEnumerator c1, IEnumerator c2)
+        {
+            while (true) {
+                var stopEither = !c1.MoveNext() || !c2.MoveNext();
+                if (stopEither)
+                    yield break;
+                else
+                    yield return null;
+            }
+        }
+    }
+}
