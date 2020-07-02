a6ce90cec10b0d338a6d994c2054c4e6a2279ead
JuanPabloH <juan@3diq.io>
2020-07-02 15:34:51 -0500

    Add behavior for splash screen and level manager to load next scene.

---
+++ Assets/Scripts/Managers/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField] private SceneReference sceneToLoad;
+    [SerializeField] private float minTime = 3.0f;
+
+    private void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    private IEnumerator LoadSceneAsync()
+    {
+       AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+       async.allowSceneActivation = false;
+       float t = 0;
+       while (async.isDone || t< minTime)
+       {
+           yield return null;
+       }
+       async.allowSceneActivation = true;
+    }
+}
---
+++ Assets/Scripts/UI/BlinkImage.cs
@@ -0,0 +1,19 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkImage : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private AnimationCurve ease;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(DOTween.ToAlpha(() => image.color, x => image.color = x, 0, 1).SetEase(ease));
+        s.Append(DOTween.ToAlpha(() => image.color, x => image.color = x, 1, 1).SetEase(ease));
+        s.SetLoops(-1);
+    }
+    
+}
