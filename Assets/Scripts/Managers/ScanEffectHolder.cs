0a641ffb0f43415b6ed59eca9051b702aaeb0f0e
JuanPabloH <juan@3diq.io>
2020-07-03 11:15:50 -0500

    Handle behavior for scan material.

--- Assets/Scripts/Managers/ScanEffectHolder.cs
+++ Assets/Scripts/Managers/ScanEffectHolder.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ScanEffectHolder : MonoBehaviour
 {
+    [SerializeField] private RuneEvent OnRuneSelected;
+    [SerializeField] private RuneEvent OnRuneConfirmed;
     [SerializeField] private Material scanMaterial;
-    
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
+    private static readonly int Activate = Shader.PropertyToID("_Activate");
+
+    private void Awake()
+    {
+        OnRuneSelected.AddListener(HandleRuneSelected);
+        OnRuneConfirmed.AddListener(HandleRuneConfirmed);
+    }
+
+    private void HandleRuneSelected(Rune rune)
+    {
+        scanMaterial.SetColor(EmissionColor,rune.Profile.Color);
+        scanMaterial.SetFloat(Activate,1.0f);
+    }
+    private void HandleRuneConfirmed(Rune rune)
+    {
+        scanMaterial.SetFloat(Activate,0.0f);
+        
+    }
+
+
+    private void OnDestroy()
+    {
+        OnRuneSelected.RemoveListener(HandleRuneSelected);
+        OnRuneConfirmed.RemoveListener(HandleRuneConfirmed);
+    }
 }
