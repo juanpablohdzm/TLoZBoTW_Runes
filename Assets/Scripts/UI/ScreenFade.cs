f1efb81f0e956f7c01002cef43c8ec6c10dbe599
JuanPabloH <juan@3diq.io>
2020-07-07 15:19:28 -0500

    Create a screen fade and remove ovr screen fade

---
+++ Assets/Scripts/UI/ScreenFade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFade : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Color fadeColor = Color.black;
+    [SerializeField] private float fadeDuration = 1.0f;
+    [SerializeField] private AnimationCurve ease;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        image.gameObject.SetActive(true);
+        image.color = fadeColor;
+    }
+
+    void Start()
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(DOTween.To(() => image.color.a, x =>
+        {
+            Color color = image.color;
+            color.a = x;
+            image.color = color;
+        }, 0.0f, fadeDuration).SetEase(ease));
+        s.OnComplete(() => { image.gameObject.SetActive(false); });
+    }
+}
