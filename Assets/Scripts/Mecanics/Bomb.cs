d11efce57445463821378003f9361f10dfc45488
JuanPabloH <juan@3diq.io>
2020-07-15 13:46:42 -0500

    Add explosion effect to remote bombs

--- Assets/Scripts/Mecanics/Bomb.cs
+++ Assets/Scripts/Mecanics/Bomb.cs
@@ -2,23 +2,58 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.VFX;
 
 [RequireComponent(typeof(Collider),typeof(Rigidbody))]
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private float explosionForce = 100.0f;
     [SerializeField] private float radius = 3.0f;
+    [SerializeField] private float destroyDelay = 2.0f;
     [SerializeField] private LayerMask interactables;
+    [SerializeField] private GameObject explosionEffectPrefab;
+   
+    private MeshRenderer mesh;
+    private Collider col;
+    private Rigidbody rb;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+            Explode();
+    }
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        col = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+
+    }
 
     public void Explode()
     {
+        GameObject effect= Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        
+        mesh.enabled = false;
+        col.isTrigger = true;
+        rb.isKinematic = true;
+        
+        
         Collider[] objects = Physics.OverlapSphere(transform.position, radius,interactables.value);
+
         foreach (var item in objects)
         {
             if (item.attachedRigidbody != null)
                 item.attachedRigidbody.AddExplosionForce(explosionForce, transform.position, radius);
         }
-        Destroy(gameObject);
+        Destroy(effect, destroyDelay);
+        Destroy(gameObject,destroyDelay);
     }
-    
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color= Color.red;
+        Gizmos.DrawSphere(transform.position,radius);
+    }
 }
